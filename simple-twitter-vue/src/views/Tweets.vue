19e809d9b68c64d00d280df230d44b7c7233a425
Jasper Chen <jasperdyb@gmail.com>
2020-05-26 22:09:57 +0800

    Tweets page setup

---
+++ simple-twitter-vue/src/views/Tweets.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .container.py-5
+      .row
+        .col-md-8
+          TweetNew
+          Tweet
+        .col-md-4
+          UserTop
+</template>
+
+<script>
+import TweetNew from "../components/TweetNew";
+import Tweet from "../components/Tweet";
+import UserTop from "../components/UserTop";
+
+export default {
+  components: {
+    TweetNew,
+    Tweet,
+    UserTop
+  }
+};
+</script>
