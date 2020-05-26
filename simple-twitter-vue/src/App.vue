efba16ad14d73b044db72f2f67eef0c47c945e82
Jasper Chen <jasperdyb@gmail.com>
2020-05-26 14:43:02 +0800

    Add component Navbar and view NotFound

--- simple-twitter-vue/src/App.vue
+++ simple-twitter-vue/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
+    <main role="main" class="mt-5 bg-white">
+      <router-view />
+    </main>
   </div>
 </template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
 
 <style>
 #app {
