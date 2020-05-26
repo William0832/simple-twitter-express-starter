8e19318fab809587a422e9b0e591558fa1f7bcd3
Jasper Chen <jasperdyb@gmail.com>
2020-05-26 22:23:07 +0800

    Reply page setup

---
+++ simple-twitter-vue/src/views/Reply.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .container.py-5
+      .row
+        .col-md-4
+          ReplyUserDashboard
+        .col-md-8
+          ReplyTweet
+          Replies
+</template>
+
+<script>
+import ReplyUserDashboard from "../components/ReplyUserDashboard";
+import ReplyTweet from "../components/ReplyTweet";
+import Replies from "../components/Replies";
+
+export default {
+  components: {
+    ReplyUserDashboard,
+    ReplyTweet,
+    Replies
+  }
+};
+</script>
