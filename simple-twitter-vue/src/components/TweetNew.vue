e136c25b0fbfd828eae2893ac56fcb611a19fc8d
Jasper Chen <jasperdyb@gmail.com>
2020-05-27 20:03:16 +0800

    Build TweetNew basic

--- simple-twitter-vue/src/components/TweetNew.vue
+++ simple-twitter-vue/src/components/TweetNew.vue
@@ -1,3 +1,47 @@
 <template lang="pug">
-  h1 Tweet New
+  form.container(@submit.stop.prevent="handleSubmit")
+    div.row.form-group.mb-4
+      textarea.col.form-control(
+        v-model="tweet"
+        rows="3"
+        name="tweet"
+        placeholder='What\'s on your mind?')
+    div.row.d-flex.justify-content-end
+      button.col-2(
+        type="submit"
+        class="btn btn-primary mr-0") Tweet
 </template>
+
+<script>
+import { v4 as uuid } from "uuid";
+
+export default {
+  props: {
+    userId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      tweet: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("after-create-tweet", {
+        commentId: uuid(), // 尚未串接 API 暫時使用隨機的 id
+        userId: this.userId,
+        tweet: this.tweet
+      });
+      this.tweet = ""; // 將表單內的資料清空
+    }
+  }
+};
+</script>
+
+<style scoped>
+textarea {
+  resize: none;
+}
+</style>
