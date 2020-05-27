8decc12d1db590d2e71da520e4dd469d1a7ca318
William0832 <squall0832@gmail.com>
2020-05-27 16:45:43 +0800

    add admin api

--- routes/apis.js
+++ routes/apis.js
@@ -6,11 +6,14 @@ const passport = require('../config/passport')
 
 const userController = require('../controllers/api/userController.js')
 const tweetController = require('../controllers/api/tweetController.js')
+const adminController = require('../controllers/api/adminController.js')
 
 const authenticated = passport.authenticate('jwt', { session: false })
 const authenticatedAdmin = (req, res, next) => {
   if (req.user) {
-    if (req.user.isAdmin) { return next() }
+    if (req.user.role === 'admin') {
+      return next()
+    }
     return res.json({ status: 'error', message: 'permission denied' })
   } else {
     return res.json({ status: 'error', message: 'permission denied' })
@@ -21,12 +24,29 @@ const authenticatedAdmin = (req, res, next) => {
 router.post('/signup', userController.signUp)
 router.post('/signin', userController.signIn)
 
-
 router.get('/', authenticated, (req, res) => res.redirect('/tweets'))
 
 //Tweets routes
 router.get('/tweets', authenticated, tweetController.getTweets)
 
-
+//Admin routes
+router.get(
+  '/admin/tweets',
+  authenticated,
+  authenticatedAdmin,
+  adminController.getTweets
+)
+router.delete(
+  '/admin/tweets/:id',
+  authenticated,
+  authenticatedAdmin,
+  adminController.deleteTweet
+)
+router.get(
+  '/admin/users',
+  authenticated,
+  authenticatedAdmin,
+  adminController.getUsers
+)
 
 module.exports = router
