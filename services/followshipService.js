6ba94d43ee078546c2ffec0beb59b225d7ccb528
Jasper Chen <jasperdyb@gmail.com>
2020-05-28 13:21:20 +0800

    Fix delete Followship to use followship id

--- services/followshipService.js
+++ services/followshipService.js
@@ -58,12 +58,7 @@ const followshipService = {
   },
   deleteFollowship: async (req, res, callback) => {
     try {
-      const followship = await Followship.findOne({
-        where: {
-          followerId: helpers.getUser(req).id,
-          followingId: req.params.followingId
-        }
-      })
+      const followship = await Followship.findByPk(req.params.followingId)
 
       if (followship) {
         await followship.destroy()
