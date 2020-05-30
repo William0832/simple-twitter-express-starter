6ba0487cb9b20c8a1b6743a0b3609c5519e251d2
Jasper Chen <jasperdyb@gmail.com>
2020-05-30 10:50:20 +0800

    Setup ReplyTweet

--- simple-twitter-vue/src/components/ReplyTweet.vue
+++ simple-twitter-vue/src/components/ReplyTweet.vue
@@ -1,3 +1,21 @@
 <template lang="pug">
-  h1 ReplyTweet
+  .container
+    .row.border.border-secondary.rounded.my-1(v-for='tweet in tweets' :key='tweet.id')
+      .col-3.d-flex.align-items-center.justify-content-center
+        img(:src="tweet.User.avatar")
+      .col-8.text-left
+        a(hred='#')
+          h3 @{{tweet.User.name}} , {{tweet.createdAt | fromNow}}
+        p
+          | {{tweet.description}}
+        .row.justify-content-start
+          .col.mw-50
+            a(href='#')
+              button.btn.btn-light Reply ({{tweet.Replies_count}})
+          .col.mw-50
+            button.btn.btn-light(@click.stop.prevent="addLike") Like ({{tweet.Likes_count}} )
 </template>
+
+<script>
+export default {};
+</script>
--- simple-twitter-vue/src/views/Reply.vue
+++ simple-twitter-vue/src/views/Reply.vue
@@ -4,7 +4,7 @@
         .col-md-4
           ReplyUserDashboard
         .col-md-8
-          ReplyTweet
+          ReplyTweet(:tweet ='tweet')
           Replies
 </template>
 
@@ -13,11 +13,46 @@ import ReplyUserDashboard from "../components/ReplyUserDashboard";
 import ReplyTweet from "../components/ReplyTweet";
 import Replies from "../components/Replies";
 
+const dummyTweet = {
+  id: 52,
+  description: "ggsfgs",
+  UserId: 1,
+  createdAt: "2020-05-29T14:57:21.000Z",
+  updatedAt: "2020-05-29T14:57:21.000Z",
+  Replies_count: 0,
+  Likes_count: 0,
+  User: {
+    id: 1,
+    name: "root",
+    avatar:
+      "https://loremflickr.com/240/240/man,women/?random=52.91279172455272"
+  },
+  Likes: {
+    UserId: null
+  },
+  isLiked: false
+};
+
 export default {
   components: {
     ReplyUserDashboard,
     ReplyTweet,
     Replies
+  },
+  data() {
+    return {
+      tweet: {},
+      replies: [],
+      user: {}
+    };
+  },
+  created() {
+    this.fetchTweet;
+  },
+  methods: {
+    fetchTweet() {
+      this.tweet = dummyTweet;
+    }
   }
 };
 </script>
