0e1f86c4d25b20d69b7f46f78f4cd2f9ec957581
Jasper Chen <jasperdyb@gmail.com>
2020-05-31 20:53:51 +0800

    Set backend route , user route on navbar

--- simple-twitter-vue/src/components/Navbar.vue
+++ simple-twitter-vue/src/components/Navbar.vue
@@ -7,10 +7,10 @@
     #navbarSupportedContent.navbar-collapse.collapse
       .ml-auto.d-flex.align-items-center
         // is user is admin
-        router-link.text-white.mr-3(to='#' v-if="currentUser.role=='admin'")
+        router-link.text-white.mr-3(:to='admin-tweets' v-if="currentUser.role=='admin'")
           | 管理員後台
         // is user is login
-        router-link.text-white.mr-3(to='#')
+        router-link.text-white.mr-3(:to="{name: 'user', params: { id: currentUser.id }}")
           | 使用者 您好
         button.btn.btn-sm.btn-outline-success.my-2.my-sm-0(type='button')
           | 登出
