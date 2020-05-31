da97dc63a826d015c06752554dfbd1f561b22dc5
Jasper Chen <jasperdyb@gmail.com>
2020-05-31 12:07:49 +0800

    Pass in dummyUsers

--- simple-twitter-vue/src/components/AdminUsersIndex.vue
+++ simple-twitter-vue/src/components/AdminUsersIndex.vue
@@ -1,3 +1,25 @@
 <template lang="pug">
-  h1 Users
+    div
+      h1 Users
+      table.table
+        thead.thead-dark
+          tr
+            th(scope='col' ) #id
+            th(scope='col' style="width: 50%") Tweet
+            th(scope='col') Posted
+            th(scope='col') Delete
+
+
+        tbody
+            tr(v-for="tweet in tweets" :key='tweet.id' style="height: 100px")
+              th(scope='row') {{tweet.id}}
+              td {{tweet.description | peek}}
+              td {{tweet.createdAt | fromNow}}
+              td.align-middle.d-flex.justify-content-center 
+                button.close(type='button', aria-label='Close')
+                  span.text-danger(aria-hidden='true') ×
 </template>
+
+<script>
+export default {};
+</script>
