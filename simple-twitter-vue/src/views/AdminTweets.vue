4eb3b946735662d4d127d80db2d4972e9019ac0a
Jasper Chen <jasperdyb@gmail.com>
2020-05-31 11:09:53 +0800

    Setup AdminTweets, AdminUsers

---
+++ simple-twitter-vue/src/views/AdminTweets.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+   .container.py-5
+    AdminNavs
+    AdminNavs
+</template>
+
+<script>
+import AdminNavs from "../components/AdminNavs";
+import AdminTweetsIndex from "../components/AdminTweetsIndex";
+
+export default {
+  components: {
+    AdminNavs,
+    AdminTweetsIndex
+  }
+};
+</script>
---
+++ simple-twitter-vue/src/views/AdminUsers.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+   .container.py-5
+    AdminNavs
+    AdminUsersIndex
+</template>
+
+<script>
+import AdminNavs from "../components/AdminNavs";
+import AdminUsersIndex from "../components/AdminUsersIndex";
+
+export default {
+  components: {
+    AdminNavs,
+    AdminUsersIndex
+  }
+};
+</script>
