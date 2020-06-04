6154f4c25c72c2563f2de9f210b9320acfeb73d4
Laura <lolacheng86@gmail.com>
2020-06-04 22:13:44 +0800

    Add chat_window and Online_User component in components, modified Tweets in views,  and install fontAwesome in vue

---
+++ simple-twitter-vue/src/components/ChatWindow.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+    #chat-popup.d-flex.flex-column.container(style="border: 2px solid black; max-width: 400px; max-height: 500px; padding: 0; background-color: white;")
+      .nav.row.no-gutters(style="background-color: black;")
+        img(src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_weight_other/1800x1200_cat_weight_other.jpg?resize=600px:*" style="width: 75px; height: 75px; border-radius: 50%; object-fit: cover;")
+        p.my-auto.ml-1.mr-auto(style="color: white;") name 
+        button.btn.cancel(type='button', @click.prevent.stop="closeWindow" style="font-weight: bold; color: white;") X
+
+      //- 顯示對話框的container 
+      .d-flex.flex-column.container.overflow-auto
+        //- 自己的對話條
+        .row.mt-3
+          div.col-5.ml-auto(style="border: 2px solid black; background-color: black;")
+            p.text-break(style="color: white") kjfhdkhsdkfkshdkfjhkghfkegehgkjeghkehghgrgsejh
+        //- 對方的對話條
+        .row.mt-3
+          div.col-5.mr-auto(style="border: 2px solid black; background-color: white;")
+            p.text-break kjfhdkhdkfhkfhkdfhkdfjfdkhjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjsgkerkh
+
+      .input-bar.row.no-gutters
+        input.col-10.form-control(type="text" placeholder='Type message..', required='')
+        button.col-2.btn(type='submit' style="border: 1px solid black;") Send
+</template>
+
+<script>
+export default {
+  props: {
+    // 訊息打包成Array
+    // messages:{
+    // type: Array,
+    // default:{}
+  },
+  methods: {
+    sendMsg() {
+      // emit訊息 
+    },
+    closeWindow() {
+      this.$emit('after-close')
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+img {
+  width: 70px;
+  height: 70px;
+  padding: 10px;
+}
+
+.frame {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background-image: "";
+  background-size: contain;
+}
+</style>
