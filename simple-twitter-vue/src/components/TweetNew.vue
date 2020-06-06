963dc01dfd153294e46665e0266ab4a7d1781097
Jasper Chen <jasperdyb@gmail.com>
2020-06-06 22:40:28 +0800

    Get google map object on TweetNew

--- simple-twitter-vue/src/components/TweetNew.vue
+++ simple-twitter-vue/src/components/TweetNew.vue
@@ -10,7 +10,7 @@
       .col.input-group 
         .input-group-prepend
           .input-group-text @
-        input.form-control(type='text', ref='search', placeholder='where are you?', v-model='address')
+        input.form-control(type='text', ref='search', placeholder='where are you?')
     .row.d-flex.justify-content-end.mt-2
       button.btn.btn-primary.col-2.mr-1(type='button', data-toggle='collapse', data-target='#collapseExample', aria-expanded='false', aria-controls='collapseExample')
         | Check-in
@@ -30,9 +30,11 @@ export default {
   data() {
     return {
       description: "",
-      address: "",
-      googleMapUrl: ""
+      googleMapSpot: {}
     };
+  },
+  mounted() {
+    window.checkAndAttachMapScript(this.initLocationSearch);
   },
   methods: {
     handleSubmit() {
@@ -40,6 +42,17 @@ export default {
         description: this.description
       });
       this.description = ""; // 將表單內的資料清空
+    },
+    initLocationSearch() {
+      let autocomplete = new window.google.maps.places.Autocomplete(
+        this.$refs.search
+      );
+      autocomplete.setFields(["name", "url"]);
+      autocomplete.setTypes(["establishment"]);
+      autocomplete.addListener("place_changed", function() {
+        this.googleMapSpot = autocomplete.getPlace();
+        console.log(this.googleMapSpot);
+      });
     }
   }
 };
