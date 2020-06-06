fee746e416bd39751cd0dd294eab68bad2706afc
Laura <lolacheng86@gmail.com>
2020-06-06 16:47:12 +0800

    Modify nav bar height in component chatWindow

--- simple-twitter-vue/src/components/ChatWindow.vue
+++ simple-twitter-vue/src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     #chat-popup.d-flex.flex-column.container(style="border: 2px solid black; max-width: 23%; max-height: 48vh; padding: 0; background-color: white;")
       .nav.row.no-gutters(style="background-color: black;")
-        img(src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_weight_other/1800x1200_cat_weight_other.jpg?resize=600px:*" style="width: 75px; height: 75px; border-radius: 50%; object-fit: cover;")
+        img(src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_weight_other/1800x1200_cat_weight_other.jpg?resize=600px:*" style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover;")
         p.my-auto.ml-1.mr-auto(style="color: white;") userid: {{chatId}}
         button.btn.cancel(type='button', @click.prevent.stop="closeWindow(chatId)" style="font-weight: bold; color: white;") X
 
