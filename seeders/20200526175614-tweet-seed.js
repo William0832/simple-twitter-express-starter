7df847571fa88f75a24a594e695580d9d829a7a8
William0832 <squall0832@gmail.com>
2020-06-08 16:23:47 +0800

    modify tweet-seed-userId for heroku

--- seeders/20200526175614-tweet-seed.js
+++ seeders/20200526175614-tweet-seed.js
@@ -1,25 +1,23 @@
-'use strict';
+'use strict'
 const faker = require('faker')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Tweets',
-      Array.from({ length: 50 }).map(d =>
-        ({
-          userId: 1 + Math.random() * 2,
-          description: faker.lorem.text().substring(0, 140),
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })
-      ), {});
-
-
-
+    return queryInterface.bulkInsert(
+      'Tweets',
+      Array.from({ length: 50 }).map((d) => ({
+        userId: 1 + Math.random() * 2 * 10,
+        description: faker.lorem.text().substring(0, 140),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Tweets', null, { truncate: true });
+    return queryInterface.bulkDelete('Tweets', null, { truncate: true })
   }
-};
+}
 
 //light up in git
