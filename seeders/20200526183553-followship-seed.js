a4fa5a62099f77d21a73033eda8d42b57ae13918
Jasper Chen <jasperdyb@gmail.com>
2020-06-08 16:24:56 +0800

    Use dynamic Id with seeds

--- seeders/20200526183553-followship-seed.js
+++ seeders/20200526183553-followship-seed.js
@@ -1,18 +1,25 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
+
+    const users = await queryInterface.sequelize.query(
+      `SELECT id from USERS;`
+    );
+
+    const userRows = users[0];
+
     return queryInterface.bulkInsert('Followships',
       Array.from({ length: 50 }).map(d => {
         let followship = {
-          followerId: 1 + Math.random() * 52,
-          followingId: 1 + Math.random() * 52,
+          followerId: userRows[Math.floor(Math.random() * userRows.length)].id,
+          followingId: userRows[Math.floor(Math.random() * userRows.length)].id,
           createdAt: new Date(),
           updatedAt: new Date(),
         }
 
         while (followship.followerId === followship.followingId) {
-          followingId = 1 + Math.random() * 52
+          followingId = userRows[Math.floor(Math.random() * userRows.length)].id
         }
 
         return followship
