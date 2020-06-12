779ff2c858bc3dca2994306ee55f680223b6bc14
William0832 <squall0832@gmail.com>
2020-06-12 20:15:23 +0800

    modify getblocks: use association get blockers info

--- services/blockService.js
+++ services/blockService.js
@@ -1,75 +1,112 @@
-const db = require('../models')
-const { Blockedship, User } = db
-const helpers = require('../_helpers')
+const db = require('../models');
+const { Blockedship, User } = db;
+const helpers = require('../_helpers');
 
+const removeKeys = (data, keys) => {
+  if (Object.keys(data).includes(...keys)) {
+    keys.forEach((k) => {
+      delete data[k];
+    });
+  } else {
+    data.forEach((d) => {
+      keys.forEach((k) => {
+        delete d[k];
+      });
+    });
+  }
+};
 const blockServices = {
+  // postman check ok~
   postBlock: async (req, res, callback) => {
     try {
+      let currentUserId = helpers.getUser(req).id;
+      // can not block user self
+      if (currentUserId === req.params.blockerId) {
+        return callback({
+          status: 'error',
+          message: '不能封鎖自己'
+        });
+      }
       let block = await Blockedship.findOne({
         where: {
           $and: {
-            createdBlockedId: helpers.getUser(req).id,
+            createdBlockedId: currentUserId,
             blockingId: req.params.blockerId
           }
         }
-      })
+      });
       if (block) {
-        console.log(block)
+        block = block.dataValues || block;
+        console.log('================= exist block: ', block);
         return callback({
           status: 'error',
-          message: ' this block is exist'
-        })
+          message: 'this block is exist'
+        });
       }
+      // create new
       block = await Blockedship.create({
-        createdBlockedId: helpers.getUser(req).id,
+        createdBlockedId: currentUserId,
         blockingId: req.params.blockerId
-      })
-      // block = block.toJSON()
-      console.log(block)
+      });
+      block = block.dataValues || block;
+      console.log('================= new block: ', block);
       callback({
         status: 'success',
-        message: `block:${block.id} was created `
-      })
+        message: `blocker: ${block.blockingId} was created `
+      });
     } catch (err) {
-      console.log(err.toString())
+      console.log(err.toString());
     }
   },
   getBlocks: async (req, res, callback) => {
     try {
-      let blocks = await Blockedship.findAll({
-        where: { createdBlockedId: helpers.getUser(req).id }
-        // include: [
-        //   {
-        //     model: User,
-        //     as: 'Blockers',
-        //     attributes: ['id', 'name', 'createdAt']
-        //   }
-        // ]
-      })
-      blocks = blocks.map((t) => ({ ...t.dataValues }))
-      callback({ blocks })
+      let currentUserId = helpers.getUser(req).id;
+      let currentUser = await User.findByPk(currentUserId, {
+        attributes: ['id', 'name'],
+        include: [
+          {
+            model: User,
+            as: 'Blockers',
+            attributes: ['id', 'name']
+          }
+        ]
+      });
+      let blockers = currentUser.toJSON().Blockers;
+      blockers = blockers.map((e) => {
+        let temp = {
+          ...e,
+          createdAt: e.Blockedship.createdAt
+        };
+        delete temp.Blockedship;
+        return temp;
+      });
+      callback({ blockers });
     } catch (err) {
-      callback({ status: 'error', message: err.toString() })
+      callback({ status: 'error', message: err.toString() });
     }
   },
   deleteBlock: async (req, res, callback) => {
     try {
+      let currentUserId = helpers.getUser(req).id;
+
       let block = await Blockedship.findOne({
         where: {
           $and: {
-            createdBlockedId: helpers.getUser(req).id,
+            createdBlockedId: currentUserId,
             blockingId: req.params.blockerId
           }
-        }
-      })
-      block.destroy()
+        },
+        attributes: ['blockingId']
+      });
+      block.destroy();
+      block = block.dataValues || block;
       callback({
         status: 'success',
-        message: `block:${block.id} was deleted `
-      })
+        message: `blocker:${block.blockingId} was deleted `
+      });
     } catch (err) {
-      callback({ status: 'error', message: err.toString() })
+      callback({ status: 'error', message: err.toString() });
     }
   }
-}
-module.exports = blockServices
+};
+module.exports = blockServices;
