d80fe21f801a1910e0822ef66154774ee6bafeb2
William0832 <squall0832@gmail.com>
2020-06-13 19:59:56 +0800

    rename blockedship-seed & add tweets-for-block-seed

---
+++ seeders/20200609205810-blockedship-seed.js
@@ -0,0 +1,26 @@
+'use strict';
+const db = require('../models');
+const User = db.User;
+// Set root block user2, find Id of user2 first
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    let user2Id = await User.findOne({ where: { name: 'user2' } });
+    user2Id = user2Id.toJSON().id;
+    return queryInterface.bulkInsert(
+      'Blockedships',
+      [
+        {
+          createdBlockedId: 1,
+          blockingId: user2Id,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Blockedships', null, {});
+  }
+};
