08707d12739d306bb2c08837383cda7b00b0382f
Jasper Chen <jasperdyb@gmail.com>
2020-06-13 20:32:22 +0800

    Show test while there's no other user online

--- simple-twitter-vue/src/components/OnlineUser.vue
+++ simple-twitter-vue/src/components/OnlineUser.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   .container.overflow-auto(style="max-height: 700px; width: 395px;")
+    p.text-left.font-weight-bold(v-if='!onlineUsers.length') There's no user online.
     button.row.btn.btn-light.my-1.p-3.d-flex.align-items-center(
     @click.prevent.stop="inviteUser(user.userId)"
     v-for='user in onlineUsers' 
@@ -17,38 +18,33 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import { Toast } from '../utils/helpers';
+import { mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState(['currentUser', 'isAuthenticated'])
+    ...mapState(["currentUser", "isAuthenticated"])
   },
   sockets: {
     getOnlineUser(users) {
       if (!users[0]) {
-        Toast.fire({
-          icon: 'warning',
-          title: '目前沒有其他使用者在線上'
-        });
         return;
       }
       this.onlineUsers = users;
     },
     getChatId({ chatId }) {
       this.chatId = chatId;
-      console.log('ococ', this.chatId);
-      console.log('onlineUsers: ', this.onlineUsers);
+      console.log("ococ", this.chatId);
+      console.log("onlineUsers: ", this.onlineUsers);
     },
     updateOnlineState(payload) {
       const { userId, isOnline } = payload;
       // 上線: 人數不多的情況，暫時利用現有的方法，刷新全部onlineUsers，
       // 但如果刷全部已經太耗費資源，可以改成渲染剛上線的人就好
-      this.$socket.emit('fetchOnlineUser', this.currentUser.id);
+      this.$socket.emit("fetchOnlineUser", this.currentUser.id);
 
       // // 離線: 從渲染物件中移除
       if (!isOnline) {
-        this.onlineUsers = this.onlineUsers.filter((e) => e.userId !== userId);
+        this.onlineUsers = this.onlineUsers.filter(e => e.userId !== userId);
       }
     }
   },
@@ -64,28 +60,28 @@ export default {
   methods: {
     async fetchOnlineUser() {
       try {
-        await this.$socket.emit('fetchOnlineUser', this.currentUser.id);
+        await this.$socket.emit("fetchOnlineUser", this.currentUser.id);
       } catch (error) {
         console.error(error);
       }
     },
     async inviteUser(userId) {
       try {
-        await this.$socket.emit('inviteUser', {
+        await this.$socket.emit("inviteUser", {
           invitedUserId: this.currentUser.id,
           guestUser: userId
         });
 
         let guestUser = this.onlineUsers.filter(
-          (user) => user.userId === userId
+          user => user.userId === userId
         )[0];
-        console.log('groupUsers', guestUser.chatId);
+        console.log("groupUsers", guestUser.chatId);
 
         setTimeout(() => {
           if (guestUser.chatId === null) guestUser.chatId = this.chatId;
 
           // 傳到父層 /views/Tweets.vue 的method afterInviteUser()
-          this.$emit('after-invite-user', userId, guestUser);
+          this.$emit("after-invite-user", userId, guestUser);
         }, 300);
       } catch (error) {
         console.log(error);
