f623a0658585d239cad740085a10476dcad4812d
William0832 <squall0832@gmail.com>
2020-06-14 18:17:24 +0800

    add return error in blockCtrl.deleBlock

--- controllers/api/blockController.js
+++ controllers/api/blockController.js
@@ -1,19 +1,23 @@
-const blockService = require('../../services/blockService.js')
+const blockService = require('../../services/blockService.js');
 const blockController = {
   postBlock: (req, res) => {
     blockService.postBlock(req, res, (data) => {
-      if (data.status === 'error') return res.status(302).json(data)
-      return res.json(data)
-    })
+      if (data.status === 'error') return res.status(302).json(data);
+      return res.status(302).json(data);
+    });
   },
 
   getBlocks: (req, res) => {
     blockService.getBlocks(req, res, (data) => {
-      if (data.status === 'error') return res.status(302).json(data)
-      return res.json(data)
-    })
+      if (data.status === 'error') return res.status(302).json(data);
+      return res.json(data);
+    });
   },
-  deleteBlock: (req, res) =>
-    blockService.deleteBlock(req, res, (data) => res.status(302).json(data))
-}
-module.exports = blockController
+  deleteBlock: (req, res) => {
+    blockService.deleteBlock(req, res, (data) => {
+      if (data.status === 'error') return res.status(302).json(data);
+      return res.status(302).json(data);
+    });
+  }
+};
+module.exports = blockController;
